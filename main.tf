503869116c9e3a10ef8b7b5f4e9ffa4d37f5a5b3
edtshuma <edtshuma@gmail.com>
2023-02-11 15:35:26 +0100

    Increase subnet ccount

--- main.tf
+++ main.tf
@@ -10,15 +10,15 @@ module "networking" {
   source   = "./networking"
   vpc_cidr = local.vpc_cidr
   max_subnets      = 20
-  public_sn_count = 1
-  private_sn_count = 1
+  public_sn_count = 2
+  private_sn_count = 2
   container_port = 80
   destination_cidr_block = "0.0.0.0/0"
   #even numbers for public subnets
   public_cidrs     = [for i in range(2, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
   #odd numbers for public subnets
   private_cidrs    = [for i in range(1, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)] 
-  
+
 
   # public_cidrs   = "${cidrsubnet(local.vpc_cidr, 8, 2)}"
   # private_cidrs    = "${cidrsubnet(local.vpc_cidr, 8, 1)}"
